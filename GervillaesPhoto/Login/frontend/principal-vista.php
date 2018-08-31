64ac7d0615c24f2683d927d03d430d4bc9cd3b94
GervillaesPhoto <gervillaesphoto@gmail.com>
2018-08-31 13:25:15 +0200

    commit

---
+++ GervillaesPhoto/Login/frontend/principal-vista.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sesión iniciada</title>
+    
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    
+    <link rel="stylesheet" href="css/style.css">
+    
+    <style>
+        
+        body{
+            background-image: url(../img/otros/otros3.jpg);
+        }
+        
+        .welcome{
+            width: 100%;
+            max-width: 600px;
+            margin: auto;
+            margin-top: 20rem;
+            background: rgba(0,0,0,0.6);
+            text-align: center;
+            padding: 20px;
+        }
+        
+        .welcome img{
+            width: 120px;
+            height: 120px;
+            text-align: center;
+        }
+
+        .welcome h1{
+            font-size: 50px;
+            color: white;
+            font-weight: 100;
+            margin-top: 20px;
+        }
+        
+        .welcome a{
+            display: block;
+            margin-top: 40px;
+            font-size: 20px;
+            padding: 10px;
+            border: 1px solid white;
+        }
+        
+        .welcome a:hover{
+            color: black;
+            background: white;
+        }
+        
+    
+    </style>
+    
+</head>
+<body>
+   
+   <div class="welcome">
+       <img src="../img/logo2.png" >
+        <h1>Bienvenido a GervillaesPhoto</h1>
+        <a href="cerrar.php">Cerrar sesión</a>
+   </div>
+   
+</body>
+</html>
